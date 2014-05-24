f2c5ad8ed9f49ca49dd4bd708a976e79f455d409
Omar Garcia <omarexperience@gmail.com>
2014-05-23 21:43:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polaris::Application.config.secret_key_base = '51da8b9b059971bb0a5df00947734157f8626caa7a94a867dec9c2b14571e2a74762ce2432635767ee5af9b0526a3db2beea84785e67b2137c66f309bd512a45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Polaris::Application.config.session_store :cookie_store, key: '_polaris_session'
