31e180c0232f7cbf6f0ad2e2a6fb12a7de623f6a
Omar Garcia <omarexperience@gmail.com>
2014-05-23 21:47:26 -0500

    added rpsec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -23,5 +23,6 @@ def secure_token
     token
   end
 end
-Polaris::Application.config.secret_key_base = secret_token
+Polaris::Application.config.secret_key_base = secure_token
+
 # Polaris::Application.config.secret_key_base = '51da8b9b059971bb0a5df00947734157f8626caa7a94a867dec9c2b14571e2a74762ce2432635767ee5af9b0526a3db2beea84785e67b2137c66f309bd512a45'
