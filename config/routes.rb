9d99821e1ee0b5517b4b2b1cd54486769d0faa9e
Omar Garcia <omarexperience@gmail.com>
2014-05-23 22:31:45 -0500

    added some partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Polaris::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root "static_pages#home"
+  match 'acercade', to: 'static_pages#about', via: :get
+  match 'contacto', to: 'static_pages#contact', via: :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
