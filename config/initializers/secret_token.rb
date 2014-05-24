cd72e0f7c006f521c36f05579dc830e6e1c85dde
Omar Garcia <omarexperience@gmail.com>
2014-05-23 21:46:40 -0500

    added secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Polaris::Application.config.secret_key_base = '51da8b9b059971bb0a5df00947734157f8626caa7a94a867dec9c2b14571e2a74762ce2432635767ee5af9b0526a3db2beea84785e67b2137c66f309bd512a45'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+Polaris::Application.config.secret_key_base = secret_token
+# Polaris::Application.config.secret_key_base = '51da8b9b059971bb0a5df00947734157f8626caa7a94a867dec9c2b14571e2a74762ce2432635767ee5af9b0526a3db2beea84785e67b2137c66f309bd512a45'
